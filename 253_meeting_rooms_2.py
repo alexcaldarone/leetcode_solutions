13365f0dd0bfa425c6266faefb480ca4e764f7d9
Alex Caldarone <cldrna_coding.is235@aleeas.com>
2025-09-14 17:03:33 +0200

    253

---
+++ 253_meeting_rooms_2.py
@@ -0,0 +1,31 @@
+"""
+Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
+Example 1:
+    Input: intervals = [[0,30],[5,10],[15,20]]
+    Output: 2
+
+Example 2:
+    Input: intervals = [[7,10],[2,4]]
+    Output: 1
+"""
+
+import heapq
+from typing import List
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        res = 0
+
+        intervals.sort()
+        print(intervals)
+        heap = []
+
+        for start, end in intervals:
+            while heap and start >= heap[0]:
+                heapq.heappop(heap) 
+            
+            heapq.heappush(heap, end)
+            res = max(res, len(heap))
+
+        return res
