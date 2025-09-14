c67018eeb901e16e0cc2de4b738ffb7c1b634edd
Alex Caldarone <cldrna_coding.is235@aleeas.com>
2025-09-14 16:17:29 +0200

    1347

---
+++ 1347_min_steps_anagram.py
@@ -0,0 +1,36 @@
+"""
+You are given two strings of the same length s and t. In one step you can choose any character of t and replace it with another character.
+Return the minimum number of steps to make t an anagram of s.
+An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+
+Example 1:
+    Input: s = "bab", t = "aba"
+    Output: 1
+    Explanation: Replace the first 'a' in t with b, t = "bba" which is anagram of s.
+
+Example 2:
+    Input: s = "leetcode", t = "practice"
+    Output: 5
+    Explanation: Replace 'p', 'r', 'a', 'i' and 'c' from t with proper characters to make t anagram of s.
+
+Example 3:
+    Input: s = "anagram", t = "mangaar"
+    Output: 0
+    Explanation: "anagram" and "mangaar" are anagrams. 
+"""
+
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        res = 0
+
+        count_t = {}
+        for let in t:
+            count_t[let] = 1 + count_t.get(let, 0)
+        
+        for let in s:
+            if let in t and count_t[let] > 0:
+                count_t[let] -= 1
+            else:
+                res += 1
+        
+        return res
