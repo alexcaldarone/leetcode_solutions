c71c8cbf7e29e95a951054ee63fc5a46c4916616
alexcaldarone <cldrna_coding.is235@aleeas.com>
2025-12-18 23:37:57 +0100

    add iterative solution to longest palindrome substring

--- leetcode/5_longest_palindromic_substring.py
+++ leetcode/5_longest_palindromic_substring.py
@@ -38,3 +38,26 @@ class Solution:
                     max_len = j - i + 1
 
         return s[start:start + max_len]
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        
+        dp = [[0] * n for _ in range(n)]
+        # dp[i, j] = 1 indicates whether s[i:j] is a palindrome
+        res_i, res_j, max_len = 0, 0, 0
+
+        for i in range(n): 
+            dp[i][i] = 1
+
+        for i in range(n-1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and (j - i <= 1 or dp[i+1][j-1] == 1):
+                    dp[i][j] = 1
+
+                    if j-i+1 > max_len:
+                        max_len = j-i+1
+                        res_i, res_j = i, j
+
+        return s[res_i:res_j+1]
