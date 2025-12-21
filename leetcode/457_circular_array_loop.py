8aa8184ae8b93553bd758d4135dac2521eeb4fd8
alexcaldarone <cldrna_coding.is235@aleeas.com>
2025-12-21 20:05:01 +0100

    457

---
+++ leetcode/457_circular_array_loop.py
@@ -0,0 +1,64 @@
+"""
+You are playing a game involving a circular array of non-zero integers nums. 
+Each nums[i] denotes the number of indices forward/backward you must move if you are located at index i:
+    If nums[i] is positive, move nums[i] steps forward, and
+    If nums[i] is negative, move abs(nums[i]) steps backward.
+Since the array is circular, you may assume that moving forward from the last element puts you on the first element, 
+and moving backwards from the first element puts you on the last element.
+A cycle in the array consists of a sequence of indices seq of length k where:
+    Following the movement rules above results in the repeating index sequence seq[0] -> seq[1] -> ... -> seq[k - 1] -> seq[0] -> ...
+    Every nums[seq[j]] is either all positive or all negative.
+    k > 1
+Return true if there is a cycle in nums, or false otherwise.
+
+Example 1:
+    Input: nums = [2,-1,1,2,2]
+    Output: true
+    Explanation: The graph shows how the indices are connected. White nodes are jumping forward, while red is jumping backward.
+    We can see the cycle 0 --> 2 --> 3 --> 0 --> ..., and all of its nodes are white (jumping in the same direction).
+
+Example 2:
+    Input: nums = [-1,-2,-3,-4,-5,6]
+    Output: false
+    Explanation: The graph shows how the indices are connected. White nodes are jumping forward, while red is jumping backward.
+    The only cycle is of size 1, so we return false.
+
+Example 3:
+    Input: nums = [1,-1,5,1,4]
+    Output: true
+    Explanation: The graph shows how the indices are connected. White nodes are jumping forward, while red is jumping backward.
+    We can see the cycle 0 --> 1 --> 0 --> ..., and while it is of size > 1, it has a node jumping forward and a node jumping backward, so it is not a cycle.
+    We can see the cycle 3 --> 4 --> 3 --> ..., and all of its nodes are white (jumping in the same direction).
+"""
+
+from typing import List
+
+class Solution:
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+        def move(i):
+            return (i + nums[i]) % len(nums)
+        
+        def find_cycle_start_i(i):
+            s, f = i, i
+            sign = nums[s]
+
+            while True:
+                # self loop
+                if move(s) == s or move(f) == f or move(move(f)) == move(f):
+                    return False
+                
+                # same sign
+                if sign*nums[s] <= 0 or sign*nums[move(f)] <= 0 or sign*nums[f] <= 0:
+                    return False
+
+                s = move(s)
+                f = move(move(f))
+
+                if s == f:
+                    return True
+        
+        for i in range(len(nums)):
+            if find_cycle_start_i(i):
+                return True
+        
+        return False
