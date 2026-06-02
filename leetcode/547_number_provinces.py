eb7d6746dc4aa51956d4793ce3075b328d5effe6
alexcaldarone <cldrna_coding.is235@aleeas.com>
2026-06-02 22:45:09 +0200

    547

---
+++ leetcode/547_number_provinces.py
@@ -0,0 +1,36 @@
+"""
+There are n cities. Some of them are connected, while some are not. 
+If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+Return the total number of provinces.
+
+Example 1:
+    Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+    Output: 2
+
+Example 2:
+    Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+    Output: 3
+"""
+
+from typing import List
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def dfs(node, matrix, visited):
+            visited.add(node)
+            for neig, conn in enumerate(matrix[node]):
+                if conn == 1 and neig not in visited:
+                    dfs(neig, matrix, visited)
+        
+        res = 0
+        n = len(isConnected)
+        visited = set()
+
+        for i in range(len(isConnected)):
+            if i not in visited:
+                res += 1
+                dfs(i, isConnected, visited)
+        
+        return res
