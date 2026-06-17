7572d3e6f45d258ee5a9abdbd4eca22172da826f
alexcaldarone <cldrna_coding.is235@aleeas.com>
2026-06-17 08:59:19 +0200

    306

---
+++ leetcode/306_additive_numbers.py
@@ -0,0 +1,52 @@
+"""
+An additive number is a string whose digits can form an additive sequence.
+A valid additive sequence should contain at least three numbers. 
+Except for the first two numbers, each subsequent number in the sequence must be the sum of the preceding two.
+Given a string containing only digits, return true if it is an additive number or false otherwise.
+Note: Numbers in the additive sequence cannot have leading zeros, so sequence 1, 2, 03 or 1, 02, 3 is invalid.
+
+Example 1:
+    Input: "112358"
+    Output: true
+    Explanation: 
+        The digits can form an additive sequence: 1, 1, 2, 3, 5, 8. 
+        1 + 1 = 2, 1 + 2 = 3, 2 + 3 = 5, 3 + 5 = 8
+
+Example 2:
+    Input: "199100199"
+    Output: true
+    Explanation: 
+        The additive sequence is: 1, 99, 100, 199. 
+        1 + 99 = 100, 99 + 100 = 199
+"""
+
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+        
+        def backtrack(curr, i):
+            nonlocal num
+            # base case
+            if i == len(num):
+                return len(curr) >= 3
+
+            for j in range(i+1, len(num)+1):
+                if num[i] == "0" and j > i+1:
+                    break
+                candidate = int(num[i:j])
+                if len(curr) >= 2:
+                    needed = curr[-1] + curr[-2]
+                    if candidate < needed:
+                        continue
+                    if candidate == needed:
+                        if backtrack(curr + [candidate], j):
+                            return True
+                    if candidate > needed:
+                        break
+                else:
+                    candidate = int(num[i:j])
+                    if backtrack(curr + [candidate], j):
+                        return True
+            
+            return False
+        
+        return backtrack([], 0)
